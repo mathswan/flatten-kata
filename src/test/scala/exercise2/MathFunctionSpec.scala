b4dde9524144b49bf53837e2a456181c455ab5eb
Matthew Snaith <fault_line@hotmail.co.uk>
2014-07-03 11:51:49 +0100

    exercise 2 added to project

---
+++ src/test/scala/exercise2/MathFunctionSpec.scala
@@ -0,0 +1,27 @@
+package exercise2
+
+import spec.UnitSpec
+
+class MathFunctionSpec extends UnitSpec {
+
+  // For the below tests you will need use techniques from exercise1 & look into the use of head and tail within Lists
+  val numbersForAction = List(List(100,5),List(50,2),List(10,1))
+
+  "MathFunction" should {
+    "add all numbers to each other from four lists" in {
+      pending //use numbersForAction
+    }
+
+    "subtract all numbers from each other from four lists" in {
+      pending //use numbersForAction
+    }
+
+    "multiply all numbers together from four lists" in {
+      pending //use numbersForAction
+    }
+
+    "divide all numbers from each other from four lists" in {
+      pending //use numbersForAction
+    }
+  }
+}
